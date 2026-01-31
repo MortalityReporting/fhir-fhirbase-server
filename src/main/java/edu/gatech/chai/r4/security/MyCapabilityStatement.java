4bc5b455199a180ec9a3d22d90f3bac225854830
Myung Choi <myungchoi@gmail.com>
2026-01-31 02:49:20 -0500

    version update

--- src/main/java/edu/gatech/chai/r4/security/MyCapabilityStatement.java
+++ src/main/java/edu/gatech/chai/r4/security/MyCapabilityStatement.java
@@ -111,7 +111,7 @@ public class MyCapabilityStatement extends ServerCapabilityStatementProvider {
          .getSoftware()
 		 .setName("MDI FHIR Server")
          .setVersion(version)
-         .setReleaseDateElement(new DateTimeType("2026-01-15"));
+         .setReleaseDateElement(new DateTimeType("2026-01-31"));
 
 		cs.setPublisher("Georgia Tech Research Institute - HEAT");
 
